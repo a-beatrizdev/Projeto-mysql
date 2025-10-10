9764924c068d6a1ef11faac3401ac9694810af39
alana <alanabeatriztobias@gmail.com>
2025-10-10 11:23:37 -0300

    update em conexão

--- conexao.py
+++ conexao.py
@@ -10,10 +10,10 @@ load_dotenv()
 def conectar():
     try:
         conexao = mysql.connector.connect(
-            host="localhost",
-            user="root",
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
-            database="faculdade"
+            database=os.getenv("DB_NAME")
         )
         cursor = conexao.cursor() 
         print("Conexão estabelecida")
@@ -21,4 +21,6 @@ def conectar():
     except Exception as erro:
         print(f"Erro de conexão: {erro}") 
         return None, None
+
     
+
